6958ef9d0142124ab2fc55c6c17c060c6a7f1482
Ameer Hamza <hhamza1136@gmail.com>
2022-07-15 01:00:46 +0500

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Ameer Hamza on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
